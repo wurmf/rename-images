43bc0874f0c378b11fcf0939786efff9fd01bfb0
Fabian Wurm <wurm.fabian@outlook.com>
2018-09-16 15:15:27 +0200

    fixed naming of multiple images

--- rename-images.sh
+++ rename-images.sh
@@ -1,20 +1,36 @@
 #!/bin/sh
 
+rename_with_extension()
+{
+    extension="${1##*.}"
+    mv $1 $2.$extension
+}
+
 for filename in $@
 do
-    extension="${filename##*.}"
     new_name=`extract-date.sh "$filename"`
 
     if [ "$new_name" = "" ]; then
-        new_name="unknown"
+        continue
     fi
 
     count=`ls | grep -c "$new_name"`
+
+    if [ "$count" -eq "1" ]
+    then
+        rename_with_extension `ls | grep $new_name` "$new_name"_1
+    fi
     
     if [ "$count" -gt "0" ]
     then
-        new_name="$new_name"_"$count"
+        new_name="$new_name"_`expr "$count" + 1`
     fi
+
+
+    rename_with_extension $filename $new_name
         
-    mv $filename $new_name.$extension
 done
+
+
+
+
