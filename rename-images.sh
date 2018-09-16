8dd33e7c485aafe7dd2adb634b297849961a6f3c
Fabian Wurm <wurm.fabian@outlook.com>
2018-09-16 14:40:42 +0200

    added error handling if no taken time is available

--- rename-images.sh
+++ rename-images.sh
@@ -3,8 +3,14 @@
 for filename in $@
 do
     extension="${filename##*.}"
-    new_name=`./extract-date.sh "$filename"`
+    new_name=`extract-date.sh "$filename"`
+
+    if [ "$new_name" = "" ]; then
+        new_name="unknown"
+    fi
+
     count=`ls | grep -c "$new_name"`
+    
     if [ "$count" -gt "0" ]
     then
         new_name="$new_name"_"$count"
